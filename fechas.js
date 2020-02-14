115e9980ee49b4d25838456527dfc9be681e870b
admo1994 <dmasache42@gmail.com>
2020-02-14 09:58:16 -0500

    fechas2

--- fechas.js
+++ fechas.js
@@ -1,3 +1,4 @@
+
 var arreglo=[
     {
     nombre :'daniel',
@@ -11,3 +12,21 @@ var arreglo=[
     console.log(usuarioencontrado);
 };
 buscarusuario(arreglo,('daniel'));
+//fechas 2
+const moment = require ('moment');
+var fecha=new Date();
+console.log(fecha);
+console.log(fecha.getDay());
+console.log(fecha.getMonth()+1);
+console.log(fecha.getUTCFullYear());
+console.log(fecha.getDay(),fecha.getMonth(),fecha.getFullYear());
+const horaactual = moment().format('hh:mm:ss');
+console.log(horaactual);
+console.log(moment().add(3,'hours').toString());
+console.log(fecha.toISOString());
+console.log(fecha.toDateString());
+console.log(fecha.toUTCString());
+console.log(fecha.getDay(),fecha.getMonth());
+console.log(moment().format('dddd').toString());
+
+
